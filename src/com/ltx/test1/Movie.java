37265c25c22d477a122d65b35efb3b160fef2bd6
李太新 <toonew@danwi.me>
2016-05-25 14:43:47 +0800

    Initial commit

---
+++ src/com/ltx/test1/Movie.java
@@ -0,0 +1,35 @@
+package com.ltx.test1;
+
+/**
+ * 电影 节目
+ * Created by Rain on 2016/5/25.
+ */
+public class Movie {
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+
+    private String title;
+    private int priceCode;
+
+    public Movie(String title, int priceCode) {
+        this.title = title;
+        this.priceCode = priceCode;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPriceCode() {
+        return priceCode;
+    }
+
+    public void setPriceCode(int priceCode) {
+        this.priceCode = priceCode;
+    }
+}
---
+++ src/com/ltx/test1/Rental.java
@@ -0,0 +1,33 @@
+package com.ltx.test1;
+
+/**
+ * 租赁
+ * Created by Rain on 2016/5/25.
+ */
+public class Rental {
+
+    private Movie movie;
+
+    private int daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+
+    public void setDaysRented(int daysRented) {
+        this.daysRented = daysRented;
+    }
+}
